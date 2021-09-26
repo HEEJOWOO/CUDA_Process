38d2b193d8fab07011bd32a7ed45a6bc2940b4ba
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-09-26 16:07:50 +0900

    Update 3_CUDA_vector_sum.cpp

--- 3_CUDA_vector_sum.cpp
+++ 3_CUDA_vector_sum.cpp
@@ -1,3 +1,12 @@
+/*
+* cudaMalloc()를 호출하여 입력 배열 dev_a와 dev_b, 결과 배열 dev_c 이 세 개의 배열 공간을 각각 디바이스에 할당
+* cudaFree()를 통한 메모리 해제
+* cudaMemcpy()를 이용하되, cudaMemcpyHostToDevice 매개변수를 통해 입력 데이터를 디바이스로 복사, cudaMemcpyDeviceToHost 매개변수를 통해 결과 데이터를 호스트로 복사
+* 세 쌍의 꺾쇠괄호 문법을 이용하여 호스트 코드인 main()함수에서 디바이스 코드인 add_gpu()함수를 실행
+* 껶쇠 괄호에 대한 추가설명
+* <<<N,M>>>는 커널의 실행 방식을 결정하는 런타임 매개 변수, N는 병렬 블록의 개수, 그 수의 블록 만큼 디바이스가 커널을 개시
+* <<<2,1>>>는 두 개의 커널 복사본을 생성하고 병렬로 개시한다는 의미 
+*/
 #include "book.h"
 #include <iostream>
 #include <cuda_runtime.h>
@@ -12,8 +21,9 @@ void add_cpu(int* a, int* b, int* c) {
 }
 
 //GPU
+//C 코드를 이용하고 함수 이름에 __global__ 수식 어구를 추가하여 디바이스에서 실행되는 add()함수를 작성
 __global__ void add_gpu(int* a, int* b, int* c) {
-	int tid = blockIdx.x;
+	int tid = blockIdx.x; //blockIdx는 CUDA 런타임의 내장 변수 중 하나
 	if (tid < N) {
 		c[tid] = a[tid] + b[tid];
 	}
